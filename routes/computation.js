a66ada3a4a45c41d7f9c4f832eb8286876dd86e1
Attuluru <143151335+Attuluru@users.noreply.github.com>
2023-10-25 23:20:12 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var ran1 = Math.random()*100;
-var ran2 = Math.random()*200;
-var v1 = 0;
-var v2 = 0;
-var v3 = 0;
-
+ 
+/* GET home page. */
 router.get('/', function(req, res, next) {
-
-    v1 = Math.tanh(ran1);
-    v2 = Math.tanh(ran2);
-    v3 = Math.tanh(ran1);
-
-    res.render(`random values are ${ran1} and ${ran2} tanh values are ${ran1} and ${v1}  ${ran2} and ${v2}, ${ran1} is ${v3}`);
-  });
-  
-  module.exports = router;
-  
+  //var  random1 = Math.random().toFixed(2)
+  var random2 = Math.random().toFixed(2)
+  var y = req.query.y;
+ 
+ 
+  if(y==undefined){
+   
+    y = random2
+  }
+  power = Math.pow(y,y).toFixed(2)
+  truncate= Math.trunc(y)
+  tanh = Math.tanh(y).toFixed(2)
+  singn = Math.sign(y).toFixed(2)
+  res.send(`Math.pow(${y},${y}): ${power} Math.trunc(${y}) is: ${truncate} Math.tanh(${y}) is :
+  ${tanh} Math.sign(${y}) is : ${singn}`);
+});
+ 
+module.exports = router;
+ 
