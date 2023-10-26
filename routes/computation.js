87e74c07433dcd17249b71f9d7b65bdeecaade5b
Attuluru <143151335+Attuluru@users.noreply.github.com>
2023-10-25 22:20:48 -0500

    commit changed

--- routes/computation.js
+++ routes/computation.js
@@ -2,16 +2,17 @@ var express = require('express');
 var router = express.Router();
 var ran1 = Math.random()*100;
 var ran2 = Math.random()*200;
+var v1 = 0;
+var v2 = 0;
 var v3 = 0;
-var v4 = 0;
-var v5 = 0;
 
 router.get('/', function(req, res, next) {
-    v3 = Math.tanh(ran1);
-    v4 = Math.tanh(ran2);
-    v5 = Math.tanh(ran1);
 
-    res.render(`random values are ${ran1} and ${ran2} tan values are ${ran1} and ${v3}  ${ran2} and ${v4}, ${ran1} is ${v5}`);
+    v1 = Math.tanh(ran1);
+    v2 = Math.tanh(ran2);
+    v3 = Math.tanh(ran1);
+
+    res.render(`random values are ${ran1} and ${ran2} tanh values are ${ran1} and ${v1}  ${ran2} and ${v2}, ${ran1} is ${v3}`);
   });
   
   module.exports = router;
