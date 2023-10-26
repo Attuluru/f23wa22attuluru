66d6b16f32bafcee4df769376db0b3681e1c0c60
Attuluru <143151335+Attuluru@users.noreply.github.com>
2023-10-25 20:57:05 -0500

    chnaged

--- routes/computation.js
+++ routes/computation.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var v1 = Math.random()*10;
-var v2 = Math.random()*20;
+var ran1 = Math.random()*100;
+var ran2 = Math.random()*200;
 var v3 = 0;
 var v4 = 0;
 var v5 = 0;
 
 router.get('/', function(req, res, next) {
-    v3 = Math.tanh(v1);
-    v4 = Math.tanh(v2);
-    v5 = Math.tanh(v1);
+    v3 = Math.tanh(ran1);
+    v4 = Math.tanh(ran2);
+    v5 = Math.tanh(ran1);
 
-    res.render(`random values are ${v1} and ${v2} tan values are ${v1} and ${v3}  ${v2} and ${v4}, ${v5} is ${v1}`);
+    res.render(`random values are ${ran1} and ${ran2} tan values are ${ran1} and ${v3}  ${ran2} and ${v4}, ${ran1} is ${v5}`);
   });
   
   module.exports = router;
